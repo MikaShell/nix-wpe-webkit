3e9bd478c23205a69f58f2e2f1b8585a4d055b9b
HumXC <71064270+HumXC@users.noreply.github.com>
2026-04-15 16:46:46 +0800

    update

--- flake.nix
+++ flake.nix
@@ -2,48 +2,44 @@
   description = "Standalone WPE WebKit package for Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
 
-      overlay = import ./overlay.nix;
+    overlay = import ./overlay.nix;
 
-      forAllSystems =
-        f:
-        nixpkgs.lib.genAttrs systems (
-          system:
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (
+        system:
           f (
             import nixpkgs {
               inherit system;
-              overlays = [ overlay ];
+              overlays = [overlay];
             }
           )
-        );
-    in
-    {
-      overlays.default = overlay;
+      );
+  in {
+    overlays.default = overlay;
 
-      packages = forAllSystems (pkgs: {
-        wpewebkit = pkgs.wpewebkit;
-        default = pkgs.wpewebkit;
-      });
+    packages = forAllSystems (pkgs: {
+      wpewebkit = pkgs.wpewebkit;
+      default = pkgs.wpewebkit;
+    });
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cachix
-            nixfmt
-          ];
-        };
-      });
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cachix
+          nixfmt
+        ];
+      };
+    });
 
-      formatter = forAllSystems (pkgs: pkgs.nixfmt);
-    };
+    formatter = forAllSystems (pkgs: pkgs.nixfmt);
+  };
 }
